b4c9ba0167fa6559f3fd62edb8246b55b217554c
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-04-05 10:16:39 -0700

    adding public routes for getting the github data. I beleive i may need to show a response or even to test for a console log

--- backend/routes/githubRoutes.js
+++ backend/routes/githubRoutes.js
@@ -2,5 +2,32 @@ import { Octokit, App } from "octokit";
 
 
 
-const ocotokit =new ocotokit({ });
+const ocotokit =new ocotokit();
 
+
+//route for the issues pull
+await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "github",
+    repo: "REPO",
+      headers: {
+    'X-GitHub-Api-Version': '2022-11-28'}
+  });
+
+
+//route for the commits pull
+  await ocotokit.request("GET /repos/{owner}/{repo}/commits", {
+    owner: 'OWNER',
+    repo: 'REPO',
+      headers: {
+    'X-GitHub-Api-Version': '2022-11-28'}
+  })
+
+
+  ///route for profile pull
+  await octokit.request('GET /search/users', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+
+  //route for repo count???
